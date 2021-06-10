a5f3168ce78106fa3b51b5819597a34415a12e7f
danielseehausen <daniel.seehausen@gmail.com>
2021-06-10 16:51:06 +0200

    minor fixes - start of documentation

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const FCTGraph = require('./src/fctGraph/FCTGraph')
+const functionalities = require('./src/functionalities')
+const slots = require('./src/slots')
 
 module.exports = {
   FCTGraph,
+  functionalities,
+  slots,
 }
--- test/seeders/index.js
+++ test/seeders/index.js
@@ -1,9 +1,9 @@
 const FCTGraphSeeder = require('./FCTGraphSeeder')
-const FunctionalitySeeders = require('./functionalities')
-const SlotSeeders = require('./slots')
+const functionalitySeeders = require('./functionalities')
+const slotSeeders = require('./slots')
 
 module.exports = {
   FCTGraphSeeder,
-  FunctionalitySeeders,
-  SlotSeeders,
+  functionalitySeeders,
+  slotSeeders,
 }
