28d2a98d76f6d149bff4158a629d2227cddbe1ab
Viktor Diezel <37934155+vdiezel@users.noreply.github.com>
2021-08-10 12:53:13 +0200

    adds missing mutator export for pushin add functions (#56)
    Co-authored-by: Viktor Diezel <viktor.diezel@ospin.de>

--- src/fctGraph/Utils/mutators/index.js
+++ src/fctGraph/Utils/mutators/index.js
@@ -1,7 +1,11 @@
 const addPushOutFctForOutSlotWhichHasNone = require('./addPushOutFctForOutSlotWhichHasNone')
 const addPushOutFctForAllOutSlotsWhichHaveNone = require('./addPushOutFctForAllOutSlotsWhichHaveNone')
+const addPushInFctForInSlotWhichHasNone = require('./addPushInFctForInSlotWhichHasNone')
+const addPushInFctForAllInSlotsWhichHaveNone = require('./addPushInFctForAllInSlotsWhichHaveNone')
 
 module.exports = {
   addPushOutFctForOutSlotWhichHasNone,
   addPushOutFctForAllOutSlotsWhichHaveNone,
+  addPushInFctForInSlotWhichHasNone,
+  addPushInFctForAllInSlotsWhichHaveNone,
 }
