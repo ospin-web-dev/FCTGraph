f9938152ba1a8b484dadf11c186cf3a3f64b59ab
Viktor Diezel <37934155+vdiezel@users.noreply.github.com>
2021-10-04 11:18:20 +0200

    fix: export mutators (#70)
    Co-authored-by: Viktor Diezel <viktor.diezel@ospin.de>

--- src/fctGraph/Utils/mutators/index.js
+++ src/fctGraph/Utils/mutators/index.js
@@ -1,11 +1,15 @@
 const addPushOutFctForOutSlotWhichHasNone = require('./addPushOutFctForOutSlotWhichHasNone')
 const addPushOutFctForAllOutSlotsWhichHaveNone = require('./addPushOutFctForAllOutSlotsWhichHaveNone')
+const addIntervalOutFctForOutSlotWhichHasNone = require('./addIntervalOutFctForOutSlotWhichHasNone')
+const addIntervalOutFctForAllOutSlotsWhichHaveNone = require('./addIntervalOutFctForAllOutSlotsWhichHaveNone')
 const addPushInFctForInSlotWhichHasNone = require('./addPushInFctForInSlotWhichHasNone')
 const addPushInFctForAllInSlotsWhichHaveNone = require('./addPushInFctForAllInSlotsWhichHaveNone')
 
 module.exports = {
   addPushOutFctForOutSlotWhichHasNone,
   addPushOutFctForAllOutSlotsWhichHaveNone,
+  addIntervalOutFctForOutSlotWhichHasNone,
+  addIntervalOutFctForAllOutSlotsWhichHaveNone,
   addPushInFctForInSlotWhichHasNone,
   addPushInFctForAllInSlotsWhichHaveNone,
 }
