3d14c679e82e47b36514f8b6015c379baff4ba42
Viktor Diezel <viktor.diezel@ospin.de>
2021-10-04 10:43:12 +0200

    adds tests for mutators

---
+++ test/fctGraph/utils/mutators/addIntervalOutFctForOutSlotWhichHasNone.test.js
@@ -0,0 +1,99 @@
+const FCTGraphSeeder = require('seeders/fctGraph/FCTGraphSeeder')
+const { TemperatureSensorSeeder, IntervalOutSeeder } = require('seeders/functionalities')
+const { IntegerInSlotSeeder, IntegerOutSlotSeeder } = require('seeders/slots')
+
+const addIntervalOutFctForOutSlotWhichHasNone = require('fctGraph/Utils/mutators/addIntervalOutFctForOutSlotWhichHasNone')
+
+describe('addIntervalOutFctForOutSlotWhichHasNone', () => {
+
+  describe('given invalid arguments', () => {
+    it('throws error if the first argument is not a fctGraph instance', () => {
+      const fctGraphData = FCTGraphSeeder.generate()
+      const outSlot = IntegerOutSlotSeeder.seedOne()
+
+      expect(
+        () => addIntervalOutFctForOutSlotWhichHasNone(fctGraphData, outSlot),
+      ).toThrow(/the first argument must be an instance of fctGraph/)
+    })
+
+    it('throws error if the slot\'s parent fct can not be found on the fctGraph', () => {
+      const fctGraph = FCTGraphSeeder.seedOne()
+      const outSlot = IntegerOutSlotSeeder.seedOne()
+
+      expect(
+        () => addIntervalOutFctForOutSlotWhichHasNone(fctGraph, outSlot),
+      ).toThrow(/parent fct can not be found/)
+    })
+
+    it('throws error if the slot is not an outslot', () => {
+      const fctGraph = FCTGraphSeeder.seedOne()
+      const inSlot = IntegerInSlotSeeder.seedOne()
+
+      expect(
+        () => addIntervalOutFctForOutSlotWhichHasNone(fctGraph, inSlot),
+      ).toThrow(/must be of type OutSlot/)
+    })
+
+    it('throws error if the slot already has a connected OutputNode fct', () => {
+      const fctGraph = FCTGraphSeeder.seedOne({
+        functionalities: [
+          TemperatureSensorSeeder.generateCelsiusFloatProducer(),
+          IntervalOutSeeder.generateFloatIntervalOutCelsius(),
+        ],
+      })
+
+      const [ temperatureSensor, intervalOutFct ] = fctGraph.functionalities
+      const { error } = temperatureSensor.outSlots[0].connectTo(intervalOutFct.inSlots[0])
+
+      expect(error).toBe(false)
+      expect(
+        () => addIntervalOutFctForOutSlotWhichHasNone(fctGraph, temperatureSensor.outSlots[0]),
+      ).toThrow(/already has a connected OutputNode/)
+    })
+
+    describe('when the addition of the new functionality fails at the connection', () => {
+      it('returns something that looks enough like a public error response', () => {
+        const fctGraph = FCTGraphSeeder.seedOne({
+          functionalities: [
+            TemperatureSensorSeeder.generate(),
+          ],
+        })
+
+        const tempSensor = fctGraph.functionalities[0]
+        const tempSensorOutSlot = tempSensor.getOutSlots()[0]
+        const errorMsg = 'no!'
+        tempSensorOutSlot._connectTo = () => { throw new Error(errorMsg) }
+
+        const { error, errorMsg: receivedErrorMsg } = (
+          addIntervalOutFctForOutSlotWhichHasNone(fctGraph, tempSensorOutSlot)
+        )
+
+        expect(error).toBe(true)
+        expect(receivedErrorMsg).toStrictEqual(errorMsg)
+      })
+    })
+  })
+
+  describe('given valid arguments', () => {
+    it('adds a corresponding intervalOut fct, which is connected to the outSlot', () => {
+      const fctGraph = FCTGraphSeeder.seedOne({
+        functionalities: [
+          TemperatureSensorSeeder.generate(),
+        ],
+      })
+
+      const tempSensor = fctGraph.functionalities[0]
+      const tempSensorOutSlot = tempSensor.getOutSlots()[0]
+
+      expect(fctGraph.functionalities).toHaveLength(1)
+
+      addIntervalOutFctForOutSlotWhichHasNone(fctGraph, tempSensorOutSlot)
+
+      expect(fctGraph.functionalities).toHaveLength(2)
+
+      const [ temperatureSensor, intervalOut ] = fctGraph.functionalities
+      expect(temperatureSensor.isConnectedToFct(intervalOut)).toBe(true)
+    })
+  })
+
+})
