bbb4a58b77acf8b5941ae1bf05678899091d8a0d
FelixMZ2018 <FelixinLondon@googlemail.com>
2021-11-03 12:12:19 +0100

    added fct to export and added exception list to one test

--- src/fctGraph/Utils/predicates/index.js
+++ src/fctGraph/Utils/predicates/index.js
@@ -1,5 +1,7 @@
 const fctGraphsAreSameWithoutIONodes = require('./fctGraphsAreSameWithoutIONodes')
+const fctsAreFunctionallyEqual = require('./fctsAreFunctionallyEqual')
 
 module.exports = {
   fctGraphsAreSameWithoutIONodes,
+  fctsAreFunctionallyEqual,
 }
