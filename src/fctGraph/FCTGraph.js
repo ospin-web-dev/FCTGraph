80d561c05bf9ad791599130162331b06a2032060
Viktor Diezel <viktor.diezel@ospin.de>
2022-09-05 11:44:44 +0200

    fix: makes "id" on the functionality graph required

--- src/fctGraph/FCTGraph.js
+++ src/fctGraph/FCTGraph.js
@@ -6,7 +6,7 @@ const Slot = require('../slots/Slot')
 const RegexUtils = require('../utils/RegexUtils')
 
 const SCHEMA = Joi.object({
-  id: Joi.string().pattern(RegexUtils.UUIDV4),
+  id: Joi.string().pattern(RegexUtils.UUIDV4).required(),
   deviceId: Joi.string().pattern(RegexUtils.UUIDV4).required(),
   deviceDefault: Joi.boolean().strict().default(false),
   name: Joi.string().min(1).max(255).required(),
